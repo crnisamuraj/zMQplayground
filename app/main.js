95aad98ed866cc6ee3cf4992e5862bc70d65862f
crnisamuraj <m1ivancevic@gmail.com>
2020-01-12 21:10:10 +0100

    test multiple consumers pull

--- app/main.js
+++ app/main.js
@@ -3,4 +3,9 @@ const consumer = require('./modules/consumer')
 
 producer.run(1)
 consumer.run(1)
-setTimeout(() => consumer.run(2), 5000)
+setTimeout(() => {
+	consumer.run(2)
+	setTimeout(() => {
+		consumer.run(3)
+	}, 5000)
+}, 5000)
